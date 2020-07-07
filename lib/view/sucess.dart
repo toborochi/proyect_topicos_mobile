8aa7966204d2783d1d2eaac4876c987285079508
Leonardo Añez Vladimirovna <toborochi98@outlook.com>
2020-07-07 01:32:43 -0400

    First commit

---
+++ lib/view/sucess.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class Sucess extends StatelessWidget {
+  const Sucess({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+    );
+  }
+}
