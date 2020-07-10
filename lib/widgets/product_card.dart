309dd1b681ef99e4ccd1756ab46f80d6f9aeb975
Leonardo Añez Vladimirovna <toborochi98@outlook.com>
2020-07-10 00:55:39 -0400

    Borrando la carpeta view

---
+++ lib/widgets/product_card.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class ProductCard extends StatefulWidget {
+  ProductCard({Key key}) : super(key: key);
+
+  @override
+  _ProductCardState createState() => _ProductCardState();
+}
+
+class _ProductCardState extends State<ProductCard> {
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+    
+      child: Text("Producto")
+      );
+  }
+}
