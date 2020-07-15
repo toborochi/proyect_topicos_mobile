b9274e9d436b9961ced9ce8bbd90a53e984f740c
cartory <xXcartoryXx@gmail.com>
2020-07-15 12:06:05 -0400

    products -view

--- lib/src/widgets/views/products_view.dart
+++ lib/src/widgets/views/products_view.dart
@@ -1,7 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
 import 'package:proyect_topicos_mobile/src/models/Product.dart';
-import 'package:proyect_topicos_mobile/src/providers/action.provider.dart';
 import 'package:proyect_topicos_mobile/src/widgets/product_card.dart';
 
 class ProductsView extends StatefulWidget {
@@ -14,17 +12,7 @@ class ProductsView extends StatefulWidget {
 class _ProductsViewState extends State<ProductsView> {
   @override
   Widget build(BuildContext context) {
-    final view = Provider.of<ActionProvider>(context);
-    if (this.widget.products == null) {
-      return StreamBuilder(
-        stream: view.getProvider(),
-        builder: (_, AsyncSnapshot<List<Product>> snapshot) {
-          return ListView(children: _buildList(snapshot.data));
-        },
-      );
-    } else {
-      return ListView(children: _buildList(this.widget.products));
-    }
+    return ListView(children: _buildList(this.widget.products));
   }
 
   List<Widget> _buildList(List<Product> data) {
