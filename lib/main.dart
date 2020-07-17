ac9576a6d2a1d0962f9f002ae7db91552f6dcc1b
cartory <xXcartoryXx@gmail.com>
2020-07-16 21:08:31 -0400

    auth

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,5 @@
-// import 'package:flutter/services.dart';
 import 'package:flutter/material.dart';
-import 'package:permission_handler/permission_handler.dart';
-import 'package:provider/provider.dart';
-import 'package:proyect_topicos_mobile/src/providers/action.provider.dart';
-import 'package:proyect_topicos_mobile/src/scaffold.dart';
-import 'package:proyect_topicos_mobile/src/widgets/views/homepage.dart';
-import 'package:proyect_topicos_mobile/src/widgets/views/products_view.dart';
+import 'package:proyect_topicos_mobile/services/authservice.dart';
 
 void main() => runApp(MyApp());
 
@@ -13,16 +7,9 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    // SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
-    Permission.microphone.request().then((value) => print(value.isGranted));
-    return ChangeNotifierProvider(
-      create: (_) => ActionProvider(HomePage()),
-      child: MaterialApp(
-        title: "ProyecTópicos Mobile",
-        debugShowCheckedModeBanner: false,
-        home: MyScaffold(),
-        darkTheme: ThemeData.dark(),
-      ),
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: AuthService().handleAuth(),
     );
   }
 }
---
+++ lib/screens/dashboard.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:proyect_topicos_mobile/services/authservice.dart';
+
+class DashboardPage extends StatefulWidget {
+  @override
+  _DashboardPageState createState() => _DashboardPageState();
+}
+
+class _DashboardPageState extends State<DashboardPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: Center(
+            child: RaisedButton(
+      child: Text('Signout'),
+      onPressed: () {
+        AuthService().signOut();
+      },
+    )));
+  }
+}
