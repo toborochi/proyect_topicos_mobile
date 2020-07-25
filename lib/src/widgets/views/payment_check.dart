cb6ffd23074af6a5d684f5c7d76f7454969753d5
Leonardo Añez Vladimirovna <toborochi98@outlook.com>
2020-07-25 00:17:36 -0400

    QuickChange

--- lib/src/widgets/views/payment_check.dart
+++ lib/src/widgets/views/payment_check.dart
@@ -1,5 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_spinkit/flutter_spinkit.dart';
+import 'package:proyect_topicos_mobile/src/models/Order.dart';
+import 'package:proyect_topicos_mobile/src/providers/orderProvider.dart';
+import 'package:proyect_topicos_mobile/src/providers/action.provider.dart';
+import 'package:provider/provider.dart';
+
 
 class PaymentCheck extends StatefulWidget {
   @override
@@ -10,14 +15,21 @@ class _PaymentCheckState extends State<PaymentCheck> {
   @override
   Widget build(BuildContext context) {
 
-
     final spinkit = SpinKitRing(
       color: Colors.white,
       size: 80.0,
     );
 
-    
+    final p = Provider.of<ActionProvider>(context).getOrder();
 
+    checkData(p).then((value) {
+        // ACA VA EL PUSH DE LA VISTA
+        if(value){
+            print("TRUE");
+        }else{
+            print("FALSE");
+        }
+    });
 
     return Container(
         color: Colors.green,
@@ -25,19 +37,28 @@ class _PaymentCheckState extends State<PaymentCheck> {
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: <Widget>[
-              Text("Verificando\nPedido",
-              textAlign: TextAlign.center,
+              Text(
+                "Verificando\nPedido",
+                textAlign: TextAlign.center,
                 style: TextStyle(
-                  color: Colors.white,
-                  fontSize: 24,
-                  fontWeight: FontWeight.bold
-                  ),
+                    color: Colors.white,
+                    fontSize: 24,
+                    fontWeight: FontWeight.bold),
               ),
               Container(
                 height: 64,
               ),
-               spinkit],
+              spinkit
+            ],
           ),
         ));
+  }
+
+  Future<bool> checkData(dynamic o) async {
+    final Map<String,dynamic> res = await OrderProvider.instance.saveOrder(o);
+    if(res["orderId"]!=null){
+      return true;
+    }
+    return false;
   }
 }
